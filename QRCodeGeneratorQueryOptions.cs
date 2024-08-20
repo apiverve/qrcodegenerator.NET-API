b2e5411eb1766f66a494c65d42672672ec8bd4e8
APIVerve Team <hello@apiverve.com>
2024-08-20 11:02:53 -0500

    Bug Fixes

---
+++ QRCodeGeneratorQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class QRCodeGeneratorQueryOptions {
+public string value { get; set; }
+public string format { get; set; }
+public int margin { get; set; }
+}
+
+}
