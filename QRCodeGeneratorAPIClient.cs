38b9361cf8210ca7a370ec55820a13310c1c9c56
APIVerve Team <hello@apiverve.com>
2024-08-20 11:02:51 -0500

    Bug Fixes

--- QRCodeGeneratorAPIClient.cs
+++ QRCodeGeneratorAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, qrcodegeneratorQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, QRCodeGeneratorQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(qrcodegeneratorQueryOptions options = null)
+        public ResponseObj Execute(QRCodeGeneratorQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(qrcodegeneratorQueryOptions options)
+        private string constructURL(QRCodeGeneratorQueryOptions options)
         {
             string url = _apiEndpoint;
 
